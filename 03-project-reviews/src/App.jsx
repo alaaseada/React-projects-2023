e3892dcbf75f3374f6e5c614f297b5ff7c3fa4ba
alaaseada <alaaseada87@gmail.com>
2023-05-13 08:14:48 +0200

    Add Reviews project

---
+++ 03-project-reviews/src/App.jsx
@@ -0,0 +1,35 @@
+import {useState} from 'react';
+import './App.css'
+import data from "./data"
+import Review from './Review'
+
+function App() {
+  const reviews = data
+  const [currentIndex, setCurrentIndex] = useState(0)
+  
+  const getRandomReview = () => {
+    let random_index = Math.floor(Math.random() * reviews.length)
+    while(random_index === currentIndex){
+      random_index = Math.floor(Math.random() * reviews.length)
+    }
+    setCurrentIndex(random_index)
+  }
+  
+  const goBack = () => {
+    setCurrentIndex(currentIndex - 1 < 0 ? reviews.length - 1 : currentIndex - 1)
+  }
+
+  const goForward = () => {
+    setCurrentIndex(currentIndex + 1 >= reviews.length ? 0 : currentIndex + 1)
+  }
+
+  const review = reviews[currentIndex]
+
+  return (
+    <main>
+    <Review review={review} onRandomSelection={getRandomReview} goBack={goBack} goForward={goForward}/>
+    </main>
+  )
+}
+
+export default App
