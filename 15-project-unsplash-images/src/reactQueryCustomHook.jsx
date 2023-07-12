9bddad60c50271fc07970eb721f68359eb89cd31
alaaseada87@gmail.com <alaaseada87@gmail.com>
2023-07-12 14:42:38 +0300

    Modify the queryFn to use axios instead of customFetch

--- 15-project-unsplash-images/src/reactQueryCustomHook.jsx
+++ 15-project-unsplash-images/src/reactQueryCustomHook.jsx
@@ -1,12 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
-import customFetch from './utils';
+import axios from 'axios';
+// import customFetch from './utils';
 
+const baseURL = 'https://api.unsplash.com';
 export const useFetchImages = (keyword) => {
   const { data, isLoading, isError } = useQuery({
     queryKey: ['filterImages', keyword],
     queryFn: async () => {
-      const url = `/search/photos?query=${keyword}&page=1`;
-      const { data } = await customFetch(url);
+      const url = `${baseURL}/search/photos?query=${keyword}&page=1&client_id=${
+        import.meta.env.VITE_ACCESS_KEY
+      }`;
+      const { data } = await axios.get(url);
       return data;
     },
   });
