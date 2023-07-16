53572e9608f49f6aeae4d69d07dca74ef6581580
Alaa Seada <alaaseada87@gmail.com>
2023-07-16 10:04:08 +0300

    Start working on Error page

--- 17-project-mixmaster/src/pages/Error.jsx
+++ 17-project-mixmaster/src/pages/Error.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import { Link, useRouteError } from 'react-router-dom';
+import not_found from '../assets/not-found.svg';
 
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+
+  return (
+    <Wrapper>
+      <div>
+        {error.status == 404 && (
+          <>
+            <img src={not_found} alt='not-found' />
+            <h3>Ohh!!!!</h3>
+            <p>We can't seem to find page you are looking for</p>
+          </>
+        )}
+        <Link to='/'>Back Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
